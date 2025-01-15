106fe394ce2bf8ff52b378eaab0db2237cb0a6f1
alikerock <ezwebpub@gmail.com>
2025-01-15 21:25:47 +0900

    Link delete

--- src/pages/PortfolioSingle.js
+++ src/pages/PortfolioSingle.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { supabase } from "../supabase";
